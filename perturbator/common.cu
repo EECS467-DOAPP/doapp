9c73b3e4bc8c87150e1f33ee56a13ad9ae6eaaab
Gregory Meyer <me@gregjm.dev>
2019-12-08 11:05:17 -0500

    macro it up :(

--- perturbator/common.cu
+++ perturbator/common.cu
@@ -1,6 +1,13 @@
 #include "common.cuh"
-//I know I know, it's duplicated code. If you wanna have it be non duplicated, feel free to use a macro. Oh, what's that? You don't wanna use a macro? That's what I thought.
-__constant__ float doapp::min_joint_angles[num_joints] = {1024.0f, 1024.0f, 1024.0f, 0.0f, 0.0f};
-__constant__ float doapp::max_joint_angles[num_joints] = {3072.0f, 3072.0f, 3072.0f, 2013.0f, 2013.0f};
-float doapp::host_min_joint_angles[num_joints] = {1024.0f, 1024.0f, 1024.0f, 0.0f, 0.0f};
-float doapp::host_max_joint_angles[num_joints] = {3072.0f, 3072.0f, 3072.0f, 2013.0f, 2013.0f};
+
+#define MIN_JOINT_ANGLES {1024.0f, 1024.0f, 1024.0f, 0.0f, 0.0f}
+#define MAX_JOINT_ANGLES {3072.0f, 3072.0f, 3072.0f, 2013.0f, 2013.0f}
+
+namespace doapp {
+
+__constant__ float min_joint_angles[num_joints] = MIN_JOINT_ANGLES;
+__constant__ float max_joint_angles[num_joints] = MAX_JOINT_ANGLES;
+float host_min_joint_angles[num_joints] = MIN_JOINT_ANGLES;
+float host_max_joint_angles[num_joints] = MAX_JOINT_ANGLES;
+
+} // namespace doapp
