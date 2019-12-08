844dc234c36b8e5bc6b19c4c7374f08f247bec27
Nathan Brown <nlbrow@umich.edu>
2019-12-07 23:35:55 -0500

    Compute velocity and acceleration for each joint
    Also beefed up the test file for better error reporting
    
    Future work:
    * compute smoothness cost.
    * Integrate with perception stack for obstacle detection
    * Integrate with Greg's work on doing FK on the GPU for collision
    detection

--- perturbator/generate_trajectories.cu
+++ perturbator/generate_trajectories.cu
@@ -2,8 +2,10 @@
 #include "common.cuh"
 #include <cassert>
 
+__constant__ float initial_waypoint[doapp::num_joints];
+__constant__ float final_waypoint[doapp::num_joints];
 __device__ unsigned int getTid();
-__device__ float getRandFloat(curandState& state, float min, float max);
+__device__ float getRandFloat(curandState* state, float min, float max);
 
 __global__ void init_cudarand(curandState* states, unsigned int num_rngs) {
     unsigned int tid = getTid();
@@ -16,14 +18,14 @@ __global__ void init_cudarand(curandState* states, unsigned int num_rngs) {
 __device__ void initalize_trajectories(unsigned int num_waypoints, unsigned int waypoint_dim, curandState* rng, float* trajectories) {
     if(threadIdx.x < num_waypoints*waypoint_dim) {
             assert(rng);
-            trajectories[threadIdx.x] = getRandFloat(*rng, doapp::min_joint_angles[threadIdx.x % waypoint_dim], doapp::max_joint_angles[threadIdx.x % waypoint_dim]);
+            trajectories[threadIdx.x] = getRandFloat(rng, doapp::min_joint_angles[threadIdx.x % waypoint_dim], doapp::max_joint_angles[threadIdx.x % waypoint_dim]);
     }
 }
 
 __device__ void generate_noise_vectors(unsigned int num_noise_vectors, unsigned int noise_vector_dim, float* noise_vectors, curandState* rng) {
     if(threadIdx.x < num_noise_vectors*noise_vector_dim) {
         assert(rng);
-        noise_vectors[threadIdx.x] = getRandFloat(*rng, doapp::min_joint_angles[threadIdx.x % noise_vector_dim], doapp::max_joint_angles[threadIdx.x % noise_vector_dim]);
+        noise_vectors[threadIdx.x] = getRandFloat(rng, doapp::min_joint_angles[threadIdx.x % noise_vector_dim], doapp::max_joint_angles[threadIdx.x % noise_vector_dim]);
     }
 }
 
@@ -34,18 +36,65 @@ __device__ void compute_noisy_trajectories(unsigned int num_noise_vectors, unsig
     }
 }
 
-__global__ void optimize_trajectories(float* trajectories, float* noise_vectors, float* noisy_trajectories, curandState* states, unsigned int num_rngs_per_trajectory, unsigned int num_waypoints, unsigned int waypoint_dim, unsigned int num_noise_vectors) {
+__device__ void score_noisy_trajectories(float* noisy_trajectories, unsigned int num_noisy_trajectories, unsigned int num_waypoints, unsigned int waypoint_dim, float* scores, float* accelerations) {
+    float waypoint[doapp::num_joints];
+    if(threadIdx.x < num_noisy_trajectories*num_waypoints) {
+        //load waypoint. TODO: first transpose noisy_trajectories such that accesses are coalesced. This should result in a decent speedup
+        unsigned int trajectory = threadIdx.x / num_waypoints;
+        unsigned int waypoint_index = threadIdx.x % num_waypoints;
+        for(unsigned int i = 0; i < waypoint_dim; ++i) {
+            waypoint[i] = noisy_trajectories[trajectory*num_waypoints*waypoint_dim + waypoint_index*waypoint_dim + i]; //currently, memory accesses between threads are strided by waypoint_dim (AKA not coalesced), and that is not good
+        }
+        float waypoint_cost = 1/2 * accelerations[trajectory] + num_collisions(waypoint, doapp::num_joints); //TODO: put in a term about violating joint limits
+        atomicAdd(scores + trajectory, waypoint_cost); //could do a list reduction, but like, this is one line that does the same thing
+    }
+}
+
+__global__ void optimize_trajectories(float* trajectories, float* noise_vectors, float* noisy_trajectories, curandState* states, float* velocities, float* accelerations, unsigned int num_rngs_per_trajectory, unsigned int num_waypoints, unsigned int waypoint_dim, unsigned int num_noise_vectors, float deltaT) {
     curandState* rng = threadIdx.x < num_rngs_per_trajectory ? states + (threadIdx.x + blockIdx.x*num_rngs_per_trajectory) : nullptr;
+    float* block_trajectories = trajectories + blockIdx.x*num_waypoints*waypoint_dim;
+    float* block_noise_vectors = noise_vectors + blockIdx.x*num_noise_vectors*waypoint_dim;
+    float* block_noisy_trajectories = noisy_trajectories + blockIdx.x*num_noise_vectors*num_waypoints*waypoint_dim;
+    float* block_velocities = velocities + blockIdx.x*num_noise_vectors*num_waypoints*waypoint_dim;
+    float* block_accelerations = accelerations + blockIdx.x*num_noise_vectors*num_waypoints*waypoint_dim;
     //TODO: have a shared memory slice of everything read/written to for better access timing
-    initalize_trajectories(num_waypoints, waypoint_dim, rng, trajectories);
-    generate_noise_vectors(num_noise_vectors, waypoint_dim, noise_vectors, rng);
-    compute_noisy_trajectories(num_noise_vectors, waypoint_dim, num_waypoints, noise_vectors, trajectories, noisy_trajectories);
+    initalize_trajectories(num_waypoints, waypoint_dim, rng, block_trajectories);
+    __syncthreads();
+    generate_noise_vectors(num_noise_vectors, waypoint_dim, block_noise_vectors, rng);
+    __syncthreads();
+    compute_noisy_trajectories(num_noise_vectors, waypoint_dim, num_waypoints, block_noise_vectors, block_trajectories, block_noisy_trajectories);
+    __syncthreads();
+    compute_velocity(block_noisy_trajectories, num_noise_vectors, num_waypoints, waypoint_dim, deltaT, block_velocities);
+    __syncthreads();
+    compute_acceleration(block_velocities, num_noise_vectors, num_waypoints, waypoint_dim, deltaT, block_accelerations);
+    //TODO: do a list reduction on accelerations (all threads in a block participate) but instead of just adding them, square individual terms first
+}
+
+__device__ void compute_acceleration(float* velocity, unsigned int num_noisy_trajectories, unsigned int num_waypoints, unsigned waypoint_dim, float deltaT, float* output) {
+    compute_derivative(velocity, num_noisy_trajectories, num_waypoints, waypoint_dim, deltaT, output);
+}
+__device__ void compute_velocity(float* input_trajectories, unsigned int num_noisy_trajectories, unsigned int num_waypoints, unsigned waypoint_dim, float deltaT, float* output) {
+    compute_derivative(input_trajectories, num_noisy_trajectories, num_waypoints, waypoint_dim, deltaT, output);
+}
+__device__ void compute_derivative(float* input, unsigned int num_noisy_trajectories, unsigned int num_waypoints, unsigned int waypoint_dim, float deltaT, float* output) {
+    if(threadIdx.x < num_noisy_trajectories*num_waypoints*waypoint_dim) {
+        unsigned int waypoint = (threadIdx.x / waypoint_dim) % num_waypoints;
+        unsigned int dim = threadIdx.x % waypoint_dim;
+        float prior_val, current_val;
+        prior_val = waypoint ? input[threadIdx.x - waypoint_dim] : initial_waypoint[dim];
+        current_val = input[threadIdx.x];
+        float val = (current_val - prior_val) / deltaT;
+        output[threadIdx.x] = val;
+    }
 }
 
 __device__ unsigned int getTid() {
     return threadIdx.x + blockIdx.x*blockDim.x;
 }
 
-__device__ float getRandFloat(curandState& state, float min, float max) {
-    return curand_uniform(&state) * (max - min + 0.999999) + min;
+__device__ float getRandFloat(curandState* state, float min, float max) {
+    return curand_uniform(state) * (max - min + 0.999999) + min;
+}
+__device__ unsigned int num_collisions(float* waypoint, unsigned int waypoint_dim) {
+    return 0; //TODO: use Greg's impl. For testing, might want to see if we can inverse the order of the trajectories based upon this or something idk
 }
--- perturbator/generate_trajectories.test.cu
+++ perturbator/generate_trajectories.test.cu
@@ -2,10 +2,31 @@
 #include "gpu_error_check.cuh"
 
 #include <algorithm>
+#include <cassert>
 #include <functional>
 #include <string>
 #include <iostream>
 #include <vector>
+
+//host helper function to take the derivative of an input sequence
+void take_derivative(float inital[], float final[], float trajectory[], unsigned int waypoint_dim, unsigned int num_waypoints, float deltaT, float output[]) {
+    //initalize first waypoint
+    for(unsigned int i = 0; i < waypoint_dim; ++i) {
+        output[i] = (trajectory[i] - inital[i]) / deltaT;
+    }
+    for(unsigned int waypoint = 1; waypoint < num_waypoints; ++waypoint) {
+        for(unsigned int dim = 0; dim < waypoint_dim; ++dim) {
+            float current_val = trajectory[(waypoint*waypoint_dim + dim)];
+            float prior_val = waypoint ? trajectory[(waypoint-1)*waypoint_dim + dim] : inital[dim];
+            output[waypoint*waypoint_dim + dim] = (current_val - prior_val) / deltaT;
+        }
+    }
+    /* shouldn't compute velocity by looking forward for last waypoint
+    for(unsigned int dim = 0; dim < waypoint_dim; ++dim) {
+        output[(num_waypoints-1)*waypoint_dim + dim] = (final[dim] - trajectory[(num_waypoints-1)*waypoint_dim + dim]) / deltaT;
+    }
+    */
+}
 
 //wrapper around device function initalize_trajectories to be unit testable
 __global__ void init_traject_kernel(unsigned int num_waypoints, unsigned int waypoint_dim, curandState* rngs, unsigned int num_rngs_per_block, float* all_trajectories) {
@@ -22,8 +43,12 @@ __global__ void gen_noise(unsigned int num_noise_vectors, unsigned int noise_vec
 }
 
 int main(int argc, char** argv) {
+    std::vector<float> initial_point{1024.0f, 1024.0f, 1024.0f, 0.0f, 0.0f};
+    std::vector<float> final_point{2000.0f, 2000.0f, 2000.0f, 892.0f, 0.0f};
+    float deltaT = 1; //1 second per waypoint
     unsigned int k = 10, n = 25, m = 6, d = 5; //TODO: have m be calculated from 1024/(n*d)
-    if(argc > 2) {
+    bool verbose = false;
+    if(argc > 1) {
         if(argv[1] == std::string("-h") || argv[1] == std::string("--help")) {
             std::cout << "Usage: " << argv[0] << " [-n <num_waypoints> | -k <num_trajectories> | -m <num_noise_vectors>] [-n <num_waypoints> | -k <num_trajectories> | -m <num_noise_vectors>] [-n <num_waypoints> | -k <num_trajectories> | -m <num_noise_vectors>]" << std::endl;
             return 0;
@@ -47,28 +72,36 @@ int main(int argc, char** argv) {
                     return 1;
                 }
                 m = std::stoi(argv[++i]);
+            } else if(argv[i] == std::string("-v")) {
+                verbose = true;
             } else {
                 std::cerr << "unrecognized argument: " << argv[i] << std::endl;
                 return 1;
             }
         }
     }
-    std::cout << "Running test with k = " << k << ", n = " << n << ", m = " << m << std::endl;
+    std::cout << "Running test with k = " << k << ", n = " << n << ", m = " << m << ", verbose: " << verbose << std::endl;
     unsigned int num_rngs = k*d*std::max(n,m);
-    float *dev_trajectories, *dev_noise_vectors, *dev_noisy_trajectories;
+    float *dev_trajectories, *dev_noise_vectors, *dev_noisy_trajectories, *dev_velocities, *dev_accelerations;
     curandState* dev_rngs;
+    gpuErrchk(cudaMemcpyToSymbol(initial_waypoint, initial_point.data(), initial_point.size() * sizeof(float), 0, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpyToSymbol(final_waypoint, final_point.data(), final_point.size() * sizeof(float), 0, cudaMemcpyHostToDevice));
     gpuErrchk(cudaMalloc(&dev_trajectories, k*n*d*sizeof(float)));
     gpuErrchk(cudaMalloc(&dev_noise_vectors, k*m*d*sizeof(float)));
     gpuErrchk(cudaMalloc(&dev_noisy_trajectories, k*m*n*d*sizeof(float)));
     gpuErrchk(cudaMalloc(&dev_rngs, num_rngs * sizeof(curandState)));
+    gpuErrchk(cudaMalloc(&dev_velocities, k*m*n*d*sizeof(float)));
+    gpuErrchk(cudaMalloc(&dev_accelerations, k*m*n*d*sizeof(float)));
 
 
     std::vector<float> host_trajectories(k*n*d);
     std::vector<float> host_noise_vectors(k*m*d);
     std::vector<float> host_noisy_trajectories(k*m*n*d);
+    std::vector<float> host_velocities(k*m*n*d);
+    std::vector<float> host_accelerations(k*m*n*d);
 
-    dim3 num_blocks(ceil(double(num_rngs)/1024.0));
-    dim3 num_threads(1024);
+    dim3 num_threads(512);
+    dim3 num_blocks(ceil(double(num_rngs)/num_threads.x));
     init_cudarand<<<num_blocks, num_threads>>>(dev_rngs, num_rngs);
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
@@ -78,7 +111,7 @@ int main(int argc, char** argv) {
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
     gpuErrchk(cudaMemcpy(host_trajectories.data(), dev_trajectories, k*n*d*sizeof(float), cudaMemcpyDeviceToHost));
-    gen_noise<<<gridDim, blockDim>>>(m, d, dev_noise_vectors, dev_rngs, d*m);
+    gen_noise<<<gridDim, blockDim>>>(m, d, dev_noise_vectors, dev_rngs, d*std::max(m, n));
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
     gpuErrchk(cudaMemcpy(host_noise_vectors.data(), dev_noise_vectors, k*m*d*sizeof(float), cudaMemcpyDeviceToHost));
@@ -87,48 +120,127 @@ int main(int argc, char** argv) {
         for(unsigned int noise = 0; noise < m; ++noise) {
             for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
                 for(unsigned int dim = 0; dim < d; ++dim) {
-                    host_noisy_trajectories[traj * (m*n*d) + noise*(n*d) + waypoint*d + d] = host_trajectories[traj*(n*d) + waypoint*d + dim] + host_noise_vectors[traj*m*d + noise*d + dim];
+                    host_noisy_trajectories[traj * (m*n*d) + noise*(n*d) + waypoint*d + dim] = host_trajectories[traj*(n*d) + waypoint*d + dim] + host_noise_vectors[traj*m*d + noise*d + dim];
                 }
             }
         }
     }
-
-    std::cout << "Inital Trajectories:" << std::endl;
-    for(unsigned int i = 0; i < k; ++i) {
-        for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
-            std::cout << '(';
-            for(unsigned int j = 0; j < d-1; ++j) {
-                std::cout << host_trajectories[i*n*d + j] << ", ";
-            }
-            std::cout << host_trajectories[(i+1)*n*d - 1] << ") ";
-        }
-        std::cout << std::endl;
+    for(unsigned int i = 0; i < k*m; ++i) {
+        take_derivative(initial_point.data(), final_point.data(), host_noisy_trajectories.data() + i*n*d, d, n, deltaT, host_velocities.data() + i*n*d);
+    }
+    for(unsigned int i = 0; i < k*m; ++i) {
+        take_derivative(initial_point.data(), final_point.data(), host_velocities.data() + i*n*d, d, n, deltaT, host_accelerations.data() + i*n*d);
     }
 
-    std::cout << "Noise vectors:" << std::endl;
-    for(unsigned int i = 0; i < k; ++i) {
-        for(unsigned int waypoint = 0; waypoint < m; ++waypoint) {
-            std::cout << '[';
-            for(unsigned int j = 0; j < d-1; ++j) {
-                std::cout << host_trajectories[i*n*d + j] << ", ";
-            }
-            std::cout << host_trajectories[(i+1)*n*d - 1] << "] ";
-        }
-        std::cout << std::endl;
-    }
     //reset the cudarand state should reset the rngs to the start of whatever sequence they were on
     init_cudarand<<<num_blocks, num_threads>>>(dev_rngs, num_rngs);
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
-    optimize_trajectories<<<gridDim, blockDim>>>(dev_trajectories, dev_noise_vectors, dev_noisy_trajectories, dev_rngs, d*std::max(n,m), n, d, m);
+    optimize_trajectories<<<gridDim, blockDim>>>(dev_trajectories, dev_noise_vectors, dev_noisy_trajectories, dev_rngs, dev_velocities, dev_accelerations, d*std::max(n,m), n, d, m, deltaT);
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
     std::vector<float> kernel_trajectories(k*n*d);
     std::vector<float> kernel_noise_vectors(k*m*d);
     std::vector<float> kernel_noisy_trajectories(k*m*n*d);
+    std::vector<float> kernel_velocities(k*m*n*d);
+    std::vector<float> kernel_accelerations(k*m*n*d);
     gpuErrchk(cudaMemcpy(kernel_trajectories.data(), dev_trajectories, k*n*d*sizeof(float), cudaMemcpyDeviceToHost));
     gpuErrchk(cudaMemcpy(kernel_noise_vectors.data(), dev_noise_vectors, k*m*d*sizeof(float), cudaMemcpyDeviceToHost));
     gpuErrchk(cudaMemcpy(kernel_noisy_trajectories.data(), dev_noisy_trajectories, kernel_noisy_trajectories.size() * sizeof(float), cudaMemcpyDeviceToHost));
+    gpuErrchk(cudaMemcpy(kernel_velocities.data(), dev_velocities, kernel_velocities.size() * sizeof(float), cudaMemcpyDeviceToHost));
+    gpuErrchk(cudaMemcpy(kernel_accelerations.data(), dev_accelerations, kernel_accelerations.size() * sizeof(float), cudaMemcpyDeviceToHost));
+    if(verbose) {
+        std::cout << "Host Initial Trajectories:" << std::endl;
+        for(unsigned int i = 0; i < k; ++i) {
+            for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
+                std::cout << '(';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << host_trajectories[i*n*d + j] << ", ";
+                }
+                std::cout << host_trajectories[(i+1)*n*d - 1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "Host Noise vectors:" << std::endl;
+        for(unsigned int i = 0; i < k; ++i) {
+            for(unsigned int waypoint = 0; waypoint < m; ++waypoint) {
+                std::cout << '[';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << host_noise_vectors[i*m*d + j] << ", ";
+                }
+                std::cout << host_noise_vectors[(i+1)*m*d - 1] << "] ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << "Host noisy trajectories:" << std::endl;
+        for(unsigned int i = 0; i < k*m; ++i) {
+            for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
+                std::cout << '(';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << host_noisy_trajectories[i*n*d + j] << ", ";
+                }
+                std::cout << host_noisy_trajectories[(i+1)*n*d - 1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "Host velocities:" << std::endl;
+        for(unsigned int i = 0; i < k*m; ++i) {
+            for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
+                std::cout << '(';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << host_velocities[i*n*d + waypoint*d + j] << ", ";
+                }
+                std::cout << host_velocities[(i+1)*n*d - 1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "Kernel Initial trajectories" << std::endl;
+        for(unsigned int i = 0; i < k; ++i) {
+            for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
+                std::cout << '(';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << kernel_trajectories[i*n*d + j] << ", ";
+                }
+                std::cout << kernel_trajectories[(i+1)*n*d - 1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "Kernel Noise vectors:" << std::endl;
+        for(unsigned int i = 0; i < k; ++i) {
+            for(unsigned int waypoint = 0; waypoint < m; ++waypoint) {
+                std::cout << '[';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << kernel_noise_vectors[i*m*d + j] << ", ";
+                }
+                std::cout << kernel_noise_vectors[(i+1)*m*d - 1] << "] ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << "Kernel noisy trajectories:" << std::endl;
+        for(unsigned int i = 0; i < k*m; ++i) {
+            for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
+                std::cout << '(';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << kernel_noisy_trajectories[i*n*d + j] << ", ";
+                }
+                std::cout << kernel_noisy_trajectories[(i+1)*n*d - 1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "Kernel velocities:" << std::endl;
+        for(unsigned int i = 0; i < k*m; ++i) {
+            for(unsigned int waypoint = 0; waypoint < n; ++waypoint) {
+                std::cout << '(';
+                for(unsigned int j = 0; j < d-1; ++j) {
+                    std::cout << kernel_velocities[i*n*d + waypoint*d + j] << ", ";
+                }
+                std::cout << kernel_velocities[(i+1)*n*d - 1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+    }
     struct limitChecker {
         int index = 0;
         bool operator()(float angle) {
@@ -137,11 +249,18 @@ int main(int argc, char** argv) {
             return result;
         }
     };
-    std::function<bool (float, float)> floatCompare = [](float lhs, float rhs) {
-        return std::abs(rhs - lhs) < 0.001f;
+    int error_call = 0;
+    std::function<bool (float, float)> floatCompare = [&](float lhs, float rhs) {
+        bool result = std::abs(rhs - lhs) < 0.01f;
+        if(!result) {
+            std::cout << "Error comparing " << lhs << " to " << rhs << std::endl;
+            std::cout << "error on call: " << error_call << std::endl;
+        }
+        ++error_call;
+        return result;
     };
     bool passed = true;
-    if(std::equal(kernel_trajectories.begin(), kernel_trajectories.end(), host_trajectories.begin(), floatCompare)) {
+    if(!std::equal(kernel_trajectories.begin(), kernel_trajectories.end(), host_trajectories.begin(), floatCompare)) {
         std::cout << "Trajectories do not match!" << std::endl;
         passed = false;
     }
@@ -149,7 +268,7 @@ int main(int argc, char** argv) {
         std::cout << "inital trajectories exceed joint limits" << std::endl;
         passed = false;
     }
-    if(std::equal(kernel_noise_vectors.begin(), kernel_noise_vectors.end(), host_noise_vectors.begin(), floatCompare)) {
+    if(!std::equal(kernel_noise_vectors.begin(), kernel_noise_vectors.end(), host_noise_vectors.begin(), floatCompare)) {
         std::cout << "noise vectors do not match!" << std::endl;
         passed = false;
     }
@@ -157,12 +276,22 @@ int main(int argc, char** argv) {
         std::cout << "noise vectors exceed joint limits" << std::endl;
         passed = false;
     }
-    if(std::equal(kernel_noisy_trajectories.begin(), kernel_noisy_trajectories.end(), host_noisy_trajectories.begin(), floatCompare)) {
-        std::cout << "They are not equal!" << std::endl;
+    if(!std::equal(kernel_noisy_trajectories.begin(), kernel_noisy_trajectories.end(), host_noisy_trajectories.begin(), floatCompare)) {
+        std::cout << "The noisy trajectories are not equal" << std::endl;
         passed = false;
     }
     if(!std::all_of(kernel_noisy_trajectories.begin(), kernel_noisy_trajectories.end(), limitChecker{})) {
         std::cout << "noisy trajectories exceed joint limits" << std::endl;
+        passed = false;
+    }
+    if(!std::equal(kernel_velocities.begin(), kernel_velocities.end(), host_velocities.begin(), floatCompare)) {
+        std::cout << "The velocities are not equal" << std::endl;
+        std::cout << "error comparing " << kernel_velocities[error_call-1] << " to " << host_velocities[error_call-1] << ", the difference is: " << std::abs(host_velocities[error_call] - kernel_velocities[error_call]) << std::endl;
+        std::cout << "prior value on host: " << host_noisy_trajectories[error_call-6] << ", prior value on kernel: " << kernel_noisy_trajectories[error_call-6] << ", current value on host: " << host_noisy_trajectories[error_call-1] << ", current value on kernel: " << kernel_noisy_trajectories[error_call-1] << std::endl;
+        passed = false;
+    }
+    if(!std::equal(kernel_accelerations.begin(), kernel_accelerations.end(), host_accelerations.begin(), floatCompare)) {
+        std::cout << "the accelerations are not equal!" << std::endl;
         passed = false;
     }
     if(passed) {
@@ -174,5 +303,7 @@ int main(int argc, char** argv) {
     cudaFree(dev_noise_vectors);
     cudaFree(dev_noisy_trajectories);
     cudaFree(dev_rngs);
+    cudaFree(dev_velocities);
+    cudaFree(dev_accelerations);
     return passed;
 }
