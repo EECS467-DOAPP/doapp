b80fbde0a893600f6be71dc64c86ffbf39bfa72b
Zhihao Ruan <ruanzh@umich.edu>
2019-12-09 01:19:03 -0500

    separated arm command from gripper command

--- dynamixel_driver/src/controller_main.cpp
+++ dynamixel_driver/src/controller_main.cpp
@@ -16,11 +16,15 @@ int main(int argc, char **argv) {
     ros::Subscriber subscribeMotorState =
         driver.n_.subscribe("/dynamixel_workbench/dynamixel_state", 1000, &DynamixelDriver::handleState, &driver);
 
-    // PART 3 -- Listening to motor command from other states
-    ros::ServiceServer serviceMotorCommand =
-        driver.n_.advertiseService("motor_command", &DynamixelDriver::handleCommand, &driver);
+    // PART 3 -- Listening to arm command from other nodes
+    ros::ServiceServer serviceArmCommand =
+        driver.n_.advertiseService("arm_command", &DynamixelDriver::handleArmCommand, &driver);
 
-    // PART 4 -- Publish motor states to joint_states to RViz
+    // PART 4 -- Listening to gripper command from other nodes
+    ros::ServiceServer serviceGripperCommand =
+        driver.n_.advertiseService("gripper_command", &DynamixelDriver::handleGripperCommand, &driver);
+
+    // PART 5 -- Publish motor states to joint_states to RViz
     ros::Publisher pub = driver.n_.advertise<sensor_msgs::JointState>("joint_states", 1000);
     pub.publish(driver.visualization_msg);
 
--- dynamixel_driver/src/demo.cpp
+++ dynamixel_driver/src/demo.cpp
@@ -3,7 +3,7 @@
 
 /**
  * @brief   This demo shows how to send commands to the driver 
- *          from other node. The message type is MotorCommand
+ *          from other node. The message type is ArmCommand
  *          with a goal position and an motor ID. The driver will
  *          set the goal position of the motor with corresponding 
  *          ID to be the value specified in the message.
@@ -19,8 +19,8 @@ namespace demo {
  * 
  * @param msg:  message to be initialized
  */
-void init(dynamixel_driver::MotorCommand &msg) {
-    for (int i = 0; i < NUM_MOTORS; ++i) {
+void init(dynamixel_driver::ArmCommand &msg) {
+    for (int i = 0; i < NUM_ARM_MOTORS; ++i) {
         msg.request.commands.push_back((i < NUM_TYPE_SWITCH) ? 2048 : 512);
     }
 }
@@ -29,7 +29,7 @@ void init(dynamixel_driver::MotorCommand &msg) {
 int main(int argc, char **argv) {
     ros::init(argc, argv, "dynamixel_driver_demo");
     ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient<dynamixel_driver::MotorCommand>("motor_command");
+    ros::ServiceClient client = n.serviceClient<dynamixel_driver::ArmCommand>("arm_command");
     ros::Rate loop_rate(50);  // 50Hz loop rate
 
     /* For MX-28 motor testing */
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
     const int32_t upper_bound = 819;
     bool increasing = true;
 
-    dynamixel_driver::MotorCommand msg;
+    dynamixel_driver::ArmCommand msg;
     demo::init(msg);
     int id = 3;
     msg.request.commands[id] = lower_bound;
@@ -63,7 +63,7 @@ int main(int argc, char **argv) {
         }
 
         if (client.call(msg)) {
-            ROS_INFO("Publishing %d to motor [%d]\n", msg.request.commands[id], id);
+            ROS_INFO("Publishing %d to arm [%d]\n", msg.request.commands[id], id);
             ROS_INFO("Motor reached goal: %d\n", msg.response.reached);
         }
 
